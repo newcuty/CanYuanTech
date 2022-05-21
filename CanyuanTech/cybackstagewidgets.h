45db72fa33193ee4d7140364ceaad715354d31b6
newcuty <chenbiaojob@163.com>
2022-05-21 15:44:55 +0800

    优化后台控件基类

--- CanyuanTech/cybackstagewidgets.h
+++ CanyuanTech/cybackstagewidgets.h
@@ -11,12 +11,14 @@ class cyBackStageWidgets : public QWidget
 public:
     explicit cyBackStageWidgets(QWidget *parent = nullptr);
 
-    virtual void init(){};
-    //初始化顶部控件,将界面分成需要按钮和不需要按钮的两类，默认不需要按钮
+    virtual void init() = 0;
+
+    //初始化顶部控件,顶部控件抽象成图标和文字这一大类
+    //将界面分成需要按钮和不需要按钮的两类，默认不需要按钮
     virtual QHBoxLayout* initTopLayout(const QString& icon_path, const QString& title);
     virtual QHBoxLayout* initTopLayout(const QString& icon_path, const QString& title,
-                                       QPushButton* button, const QString& button_name,
-                                       const QString& button_icon);
+                                       QPushButton* button, const QString& button_icon);
+
 
 signals:
 
